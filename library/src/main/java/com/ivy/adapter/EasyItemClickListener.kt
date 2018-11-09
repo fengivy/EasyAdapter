645170e38c6c0bc7f01635e1d307bb335cb08b86
fengivy <jsggl@vip.qq.com>
2018-11-09 16:00:54 +0800

    1.optimize code

--- library/src/main/java/com/ivy/adapter/EasyItemClickListener.kt
+++ library/src/main/java/com/ivy/adapter/EasyItemClickListener.kt
@@ -42,7 +42,7 @@ class EasyItemClickListener : RecyclerView.SimpleOnItemTouchListener() {
                         val adapter = rv.adapter as EasyAdapter
                         ids.forEach {
                             val child = viewHolder.getView<View>(it)
-                            if (child!=null&&child.isEnabled&&judgeIsTouchInView(child,e)){
+                            if (child.isEnabled&&judgeIsTouchInView(child,e)){
                                 dealLongClick(viewHolder,position,adapter,false,child)
                                 return
                             }
@@ -78,42 +78,47 @@ class EasyItemClickListener : RecyclerView.SimpleOnItemTouchListener() {
 
     private fun dealClick(holder: EasyViewHolder,position:Int, adapter: EasyAdapter, isClickItem:Boolean, child:View ?= null){
         val viewTypeInterface = adapter.getItemTypeInterfaceByPosition(position)
-        if (viewTypeInterface is EasyHeaderAndFooterViewType){
-            if (isClickItem){
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return,holder)
-            }
-        }else if (viewTypeInterface is EasyEmptyViewType){
-            if (isClickItem){
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return,holder)
-            }
-        }else if(viewTypeInterface is EasyLoadingMoreViewType){
-            if (isClickItem){
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return,holder)
-            }
-        }else{
-            if (isClickItem){
-                viewTypeInterface.onClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()])
-                viewTypeInterface.onClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()],holder)
-                if (viewTypeInterface.isOpenSingleChooseByItem()) {
-                    adapter.clickSingleChoose(adapter.getData<Any>()[position - adapter.getHeaderCount()])
+        when (viewTypeInterface) {
+            is EasyHeaderAndFooterViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child,holder)
                 }
-            }else{
-                viewTypeInterface.onClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return)
-                viewTypeInterface.onClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return,holder)
-                if (viewTypeInterface.isOpenSingleChooseByChild(child.id)){
-                    adapter.clickSingleChoose(adapter.getData<Any>()[position-adapter.getHeaderCount()])
+            }
+            is EasyEmptyViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child,holder)
+                }
+            }
+            is EasyLoadingMoreViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onClickChild(viewTypeInterface.bean?:return,child,holder)
+                }
+            }
+            else -> {
+                if (isClickItem){
+                    viewTypeInterface.onClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()])
+                    viewTypeInterface.onClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()],holder)
+                    if (viewTypeInterface.isOpenSingleChooseByItem()) {
+                        adapter.clickSingleChoose(adapter.getData<Any>()[position - adapter.getHeaderCount()])
+                    }
+                }else{
+                    viewTypeInterface.onClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return)
+                    viewTypeInterface.onClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child,holder)
+                    if (viewTypeInterface.isOpenSingleChooseByChild(child.id)){
+                        adapter.clickSingleChoose(adapter.getData<Any>()[position-adapter.getHeaderCount()])
+                    }
                 }
             }
         }
@@ -121,37 +126,42 @@ class EasyItemClickListener : RecyclerView.SimpleOnItemTouchListener() {
 
     private fun dealLongClick(holder: EasyViewHolder,position:Int, adapter: EasyAdapter, isClickItem:Boolean, child:View ?= null){
         val viewTypeInterface = adapter.getItemTypeInterfaceByPosition(position)
-        if (viewTypeInterface is EasyHeaderAndFooterViewType){
-            if (isClickItem){
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return,holder)
+        when (viewTypeInterface) {
+            is EasyHeaderAndFooterViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child,holder)
+                }
             }
-        }else if (viewTypeInterface is EasyEmptyViewType){
-            if (isClickItem){
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return,holder)
+            is EasyEmptyViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child,holder)
+                }
             }
-        }else if(viewTypeInterface is EasyLoadingMoreViewType){
-            if (isClickItem){
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
-                viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
-            }else{
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
-                viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return,holder)
+            is EasyLoadingMoreViewType -> {
+                if (isClickItem){
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return)
+                    viewTypeInterface.onLongClickItem(viewTypeInterface.bean?:return,holder)
+                }else{
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child?:return)
+                    viewTypeInterface.onLongClickChild(viewTypeInterface.bean?:return,child,holder)
+                }
             }
-        }else{
-            if (isClickItem){
-                viewTypeInterface.onLongClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()])
-                viewTypeInterface.onLongClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()],holder)
-            }else{
-                viewTypeInterface.onLongClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return)
-                viewTypeInterface.onLongClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return,holder)
+            else -> {
+                if (isClickItem){
+                    viewTypeInterface.onLongClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()])
+                    viewTypeInterface.onLongClickItem(adapter.getData<Any>()[position-adapter.getHeaderCount()],holder)
+                }else{
+                    viewTypeInterface.onLongClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child?:return)
+                    viewTypeInterface.onLongClickChild(adapter.getData<Any>()[position-adapter.getHeaderCount()],child,holder)
+                }
             }
         }
     }
